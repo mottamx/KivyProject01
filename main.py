371b94e7600d1f1112fc05c49ef84dcf5f73d77a
mottamx <cmottaa@gmail.com>
2024-01-12 23:33:18 -0600

    Top menu layout
    Still TBD functionality, only template so far.

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
 
-class Screen1(Screen):
-    pass
-
-class Screen2(Screen):
+class TemplateScreen(Screen):
     pass
 
 class MainApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(Screen1(name="screen1"))
-        sm.add_widget(Screen2(name="screen2"))
+        sm.add_widget(TemplateScreen(name='template'))
         return sm
 
-# class MainWidget(Widget):
-#     def __init__(self, **kwargs):
-#         super(MainWidget, self).__init__(**kwargs)
-
 if __name__ == '__main__':
-    app = MainApp()
-    app.run()
+    MainApp().run()
