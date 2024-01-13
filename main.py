5b3d27afadb86dede2a359889710196ccf9e21e8
mottamx <cmottaa@gmail.com>
2024-01-12 20:52:03 -0600

    Screen transition
    Transition between two screens, dummy buttons.

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+class Screen1(Screen):
+    pass
+
+class Screen2(Screen):
+    pass
 
 class MainApp(App):
     def build(self):
-        pass
+        sm = ScreenManager()
+        sm.add_widget(Screen1(name="screen1"))
+        sm.add_widget(Screen2(name="screen2"))
+        return sm
 
-class MainWidget(Widget):
-    def __init__(self, **kwargs):
-        super(MainWidget, self).__init__(**kwargs)
+# class MainWidget(Widget):
+#     def __init__(self, **kwargs):
+#         super(MainWidget, self).__init__(**kwargs)
 
 if __name__ == '__main__':
     app = MainApp()
