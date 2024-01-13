e8c3d24d1e6a7e2b22e5a2e4f527a9175f10ddd2
mottamx <cmottaa@gmail.com>
2024-01-13 02:15:14 -0600

    Fixed the TopWidget
    Working on the ConfigScreen (2). Top Widget only has two buttons, changes correctly between Screens

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+# main.py
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.boxlayout import BoxLayout
 
-class TemplateScreen(Screen):
+class MenuWidget(BoxLayout):
+    pass
+
+class MainScreen(Screen):
+    pass
+
+class ConfigScreen(Screen):
     pass
 
 class MainApp(App):
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(TemplateScreen(name='template'))
+        sm = ScreenManager(transition=NoTransition())
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(ConfigScreen(name='config'))
         return sm
 
 if __name__ == '__main__':
